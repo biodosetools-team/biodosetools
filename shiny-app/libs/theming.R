4009989a72c433063473f3e3466204cad37f6daa
Alfredo Hernández <aldomann.designs@gmail.com>
2018-10-17 06:23:42 +0200

    Added basic theming support

---
+++ shiny-app/libs/theming.R
@@ -0,0 +1,137 @@
+# Logo -----------------------------------------------------
+
+logo_biodose <- shinyDashboardLogoDIY(
+
+  boldText = "Biodose"
+  ,mainText = "Tool"
+  ,textSize = 16
+  ,badgeText = "ALPHA"
+  ,badgeTextColor = "white"
+  ,badgeTextSize = 2
+  ,badgeBackColor = "#311B92"
+  ,badgeBorderRadius = 3
+
+)
+
+
+# Theme ####
+theme_biodose <- shinyDashboardThemeDIY(
+
+  ### general
+  appFontFamily = "Sans"
+  ,appFontColor = "rgb(0,0,0)"
+  ,bodyBackColor = "rgb(255,255,255)"
+
+  ### header
+  ,logoBackColor = "rgb(91,94,168)"
+
+  ,headerButtonBackColor = "rgb(91,94,168)"
+  ,headerButtonIconColor = "rgb(255,255,255)"
+  ,headerButtonBackColorHover = "rgb(58,61,141)"
+  ,headerButtonIconColorHover = "rgb(0,0,0)"
+
+  ,headerBackColor = "rgb(91,94,168)"
+  ,headerBoxShadowColor = "rgb(220,220,220)"
+  ,headerBoxShadowSize = "2px 3px 2px"
+
+  ### sidebar
+  ,sidebarBackColor = cssGradientThreeColors(
+    direction = "right"
+    ,colorStart = "rgb(241,241,241)"
+    ,colorMiddle = "rgb(237,237,237)"
+    ,colorEnd = "rgb(210,210,210)"
+    ,colorStartPos = 0
+    ,colorMiddlePos = 97
+    ,colorEndPos = 100
+  )
+  ,sidebarPadding = 0
+
+  ,sidebarMenuBackColor = "transparent"
+  ,sidebarMenuPadding = 0
+  ,sidebarMenuBorderRadius = 0
+
+  ,sidebarShadowRadius = "0px 0px 0px"
+  ,sidebarShadowColor = ""
+
+  ,sidebarUserTextColor = "rgb(0,0,0)"
+
+  ,sidebarSearchBackColor = "rgb(255,255,255)"
+  ,sidebarSearchIconColor = "rgb(91,94,168)"
+  ,sidebarSearchBorderColor = "rgb(210,210,210)"
+
+  ,sidebarTabTextColor = "rgb(0,0,0)"
+  ,sidebarTabTextSize = 13
+  ,sidebarTabBorderStyle = "none"
+  ,sidebarTabBorderColor = ""
+  ,sidebarTabBorderWidth = 0
+
+  ,sidebarTabBackColorSelected = cssGradientThreeColors(
+    direction = "down"
+    ,colorStart = "rgb(193,193,193)"
+    ,colorMiddle = "rgb(216,216,216)"
+    ,colorEnd = "rgb(218,218,218)"
+    ,colorStartPos = 0
+    ,colorMiddlePos = 5
+    ,colorEndPos = 100
+  )
+  ,sidebarTabTextColorSelected = "rgb(91,94,168)"
+  ,sidebarTabRadiusSelected = "0px"
+
+  ,sidebarTabBackColorHover = cssGradientThreeColors(
+    direction = "right"
+    ,colorStart = "rgb(230,230,230)"
+    ,colorMiddle = "rgb(225,225,225)"
+    ,colorEnd = "rgb(210,210,210)"
+    ,colorStartPos = 0
+    ,colorMiddlePos = 97
+    ,colorEndPos = 100
+  )
+  ,sidebarTabTextColorHover = "rgb(0,0,0)"
+  ,sidebarTabBorderStyleHover = "none"
+  ,sidebarTabBorderColorHover = ""
+  ,sidebarTabBorderWidthHover = 0
+  ,sidebarTabRadiusHover = "0px"
+
+  ### boxes
+  ,boxBackColor = "rgb(248,248,248)"
+  ,boxBorderRadius = 5
+  ,boxShadowSize = "none"
+  ,boxShadowColor = ""
+  ,boxTitleSize = 18
+  ,boxDefaultColor = "rgb(225,225,225)"
+  ,boxPrimaryColor = "rgb(95,155,213)"
+  ,boxSuccessColor = "rgb(112,173,71)"
+  ,boxWarningColor = "rgb(237,125,49)"
+  ,boxDangerColor = "rgb(232,76,34)"
+
+  ,tabBoxTabColor = "rgb(255,255,255)"
+  ,tabBoxTabTextSize = 14
+  ,tabBoxTabTextColor = "rgb(0,0,0)"
+  ,tabBoxTabTextColorSelected = "rgb(91,94,168)"
+  ,tabBoxBackColor = "rgb(255,255,255)"
+  ,tabBoxHighlightColor = "rgb(210,210,210)"
+  ,tabBoxBorderRadius = 0
+
+  ### inputs
+  ,buttonBackColor = "rgb(240,240,240)"
+  ,buttonTextColor = "rgb(80,80,80)"
+  ,buttonBorderColor = "rgb(185,185,185)"
+  ,buttonBorderRadius = 5
+
+  ,buttonBackColorHover = "rgb(227,227,227)"
+  ,buttonTextColorHover = "rgb(80,80,80)"
+  ,buttonBorderColorHover = "rgb(210,210,210)"
+
+  ,textboxBackColor = "rgb(255,255,255)"
+  ,textboxBorderColor = "rgb(210,210,210)"
+  ,textboxBorderRadius = 0
+  ,textboxBackColorSelect = "rgb(255,255,255)"
+  ,textboxBorderColorSelect = "rgb(210,210,210)"
+
+  ### tables
+  ,tableBackColor = "rgb(255,255,255)"
+  ,tableBorderColor = "rgb(235,235,235)"
+  ,tableBorderTopSize = 1
+  ,tableBorderRowSize = 1
+
+)
--- shiny-app/server.R
+++ shiny-app/server.R
@@ -4,197 +4,261 @@
 # library(dplyr)
 # library(xts)
 library(shinyjs)
+source("libs/dashboardthemes.R")
 
 # Server ---------------------------------------------------
 
 server <- function(input, output) {
-	source("libs/grind.R") # For phase portrait
-	source("libs/cube.R")  # For 3D trajectories
-
-	# Dynamic plot list
-	# observeEvent(input$run_sims, {
-	output$ui_plots <- renderUI({
-		out <- list()
-		if (length(input$`plots_checkbox`)==0){return(NULL)}
-
-		for (i in input$`plots_checkbox`){
-			# The name of the repl_* renderPlot()s must match the checkboxGroupInput()s
-			out[[i]] <- plotOutput(outputId = paste0(input$sidebarmenu, "_", i))
-		}
-		return(out)
-		# })
-	})
-
-	# TS Replication ####
-	output$replication_time_series <- renderPlot({
-		model <- function(t, state, parms) {
-			with(as.list(c(state,parms)), {
-				dR1 <- kappa * RNA1 * replicase * (1 - RNA1) - gamma * RNA1
-				dp1 <-  alpha * RNA1 * (1 - replicase) - sigma * replicase
-				return(list(c(dR1, dp1)))
-			})
-		}
-
-		p <- c(kappa = input$slider_kappa,
-					 alpha = input$slider_alpha,
-					 gamma = input$slider_gamma,
-					 sigma = input$slider_sigma)
 
 
-		s <- c(RNA1 = input$slider_r1,
-					 replicase = 0)
+  output$table <- renderTable({
+    dose <- as.numeric(unlist(strsplit(input$dose,",")))
+    ab <- as.numeric(unlist(strsplit(input$cells,",")))
+    cells <- as.numeric(unlist(strsplit(input$ab,",")))
 
-		run(tstep = 0.5, state = s, odes = model, parms = p)
-	})
+    data.frame(dose,ab,cells)
+  })
 
-	# PP Replication ####
-	output$replication_phase_port <- renderPlot({
-		model <- function(t, state, parms) {
-			with(as.list(c(state,parms)), {
-				dR1 <- kappa * RNA1 * replicase * (1 - RNA1) - gamma * RNA1
-				dp1 <-  alpha * RNA1 * (1 - replicase) - sigma * replicase
-				return(list(c(dR1, dp1)))
-			})
-		}
+  output$workbench<-renderPrint({
 
-		p <- c(kappa = input$slider_kappa,
-					 alpha = input$slider_alpha,
-					 gamma = input$slider_gamma,
-					 sigma = input$slider_sigma)
+    dose <- as.numeric(unlist(strsplit(input$dose,",")))
+    ab <- as.numeric(unlist(strsplit(input$cells,",")))
+    cells <- as.numeric(unlist(strsplit(input$ab,",")))
+
+    # kurvendaten<-data.frame(dose,ab,cells)
+    # print(kurvendaten)
+
+    x0<-cells
+    x1<-cells*dose
+    x2<-cells*dose*dose
+    modelldaten<-list(x0,x1,x2,ab)
+    # print(modelldaten)
+
+    #result<-glm(ab ~  -1 + x1+x2,family=poisson(link = "identity"), data=modelldaten)
+    # result <- glm(ab ~ -1 + x0+x1+x2, family = poisson(link = "identity"), data= modelldaten)
+    result <- lm(ab ~ -1 + x0+x1+x2)
+    smry<-summary(result,correlation=TRUE)
+    smry$coefficients
+    smry$correlation
+
+    corma<-smry$correlation
+    bstat<-smry$coefficients
+    seb<-bstat[,3]
+    vakoma<-corma*outer(seb,seb)
+    vakoma<-vcov(result)
+
+    cat("\n")
+    print("Result of curve fit 'result'")
+    # print("-------------------")
+    print(result)
+
+    cat("\n")
+    print("Coefficients 'bstat'")
+    print(bstat)
+
+    cat("\n")
+    print("variance-covariance matrix 'vakoma'")
+    print(vakoma)
+
+    cat("\n")
+    print("correlation matrix 'corma'")
+    print(corma)
+  }
+  )
 
 
-		s <- c(RNA1 = input$slider_r1,
-					 replicase = 0)
 
-		plane(x = "RNA1", y = "replicase", tstep = 0.5, state = s, odes = model, parms = p, portrait = T)
-	})
+  # Legacy Code ####
 
-	# TS Bipartite ####
-	output$bipartite_time_series <- renderPlot({
-		model <- function(t, state, parms) {
-			with(as.list(c(state,parms)), {
-				dR1 <- kappa1 * RNA1 * replicase * (1 - RNA1- RNA2) - gamma1 * RNA1 - epsilon1 * RNA1 * coat
-				dR2 <- kappa2 * RNA2 * replicase * (1 - RNA1- RNA2) - gamma2 * RNA2 - epsilon2 * RNA2 * coat
-				dp1 <-  alpha * RNA1 * (1 - replicase - coat) - sigma1 * replicase
-				ds1 <-  beta * RNA2 * (1 - replicase - coat) - sigma2 * coat
-				dv1 <- epsilon1 * RNA1 * coat - delta1 * virion1
-				dv2 <- epsilon2 * RNA2 * coat - delta2 * virion2
-				return(list(c(dR1, dR2, dp1, ds1, dv1, dv2)))
-			})
-		}
+  source("libs/grind.R") # For phase portrait
+  source("libs/cube.R")  # For 3D trajectories
 
-		p <- c(
-			kappa1   = input$slider_kappa_bi,   # default: 1
-			kappa2   = input$slider_kappa_bi,
-			alpha    = input$slider_alpha_bi,   # default: 1
-			beta     = input$slider_alpha_bi,
-			gamma1   = input$slider_gamma_bi,   # default: 0.1
-			gamma2   = input$slider_gamma_bi,
-			sigma1   = input$slider_sigma_bi,   # default: 0.1
-			sigma2   = input$slider_sigma_bi,
-			epsilon1 = input$slider_epsilon_bi, # default: 0.1
-			epsilon2 = input$slider_epsilon_bi,
-			delta1   = input$slider_delta_bi,   # default: 0.1
-			delta2   = input$slider_delta_bi
-		)
 
-		s <- c(
-			RNA1 = input$slider_r1_bi,
-			RNA2 = input$slider_r2_bi,
-			replicase = 0,
-			coat = 0,
-			virion1 = 0,
-			virion2 = 0
-		)
 
-		run(tstep = 0.5, state = s, odes = model, parms = p)
-	})
+  # Dynamic plot list
+  # observeEvent(input$run_sims, {
+  output$ui_plots <- renderUI({
+    out <- list()
+    if (length(input$`plots_checkbox`)==0){return(NULL)}
 
-	# Traj Bipartite ####
-	output$bipartite_3d_traj <- renderPlot({
-		model <- function(t, state, parms) {
-			with(as.list(c(state,parms)), {
-				dR1 <- kappa1 * RNA1 * replicase * (1 - RNA1- RNA2) - gamma1 * RNA1 - epsilon1 * RNA1 * coat
-				dR2 <- kappa2 * RNA2 * replicase * (1 - RNA1- RNA2) - gamma2 * RNA2 - epsilon2 * RNA2 * coat
-				dp1 <-  alpha * RNA1 * (1 - replicase - coat) - sigma1 * replicase
-				ds1 <-  beta * RNA2 * (1 - replicase - coat) - sigma2 * coat
-				dv1 <- epsilon1 * RNA1 * coat - delta1 * virion1
-				dv2 <- epsilon2 * RNA2 * coat - delta2 * virion2
-				return(list(c(dR1, dR2, dp1, ds1, dv1, dv2)))
-			})
-		}
+    for (i in input$`plots_checkbox`){
+      # The name of the repl_* renderPlot()s must match the checkboxGroupInput()s
+      out[[i]] <- plotOutput(outputId = paste0(input$sidebarmenu, "_", i))
+    }
+    return(out)
+    # })
+  })
 
-		p <- c(
-			kappa1   = input$slider_kappa_bi,   # default: 1
-			kappa2   = input$slider_kappa_bi,
-			alpha    = input$slider_alpha_bi,   # default: 1
-			beta     = input$slider_alpha_bi,
-			gamma1   = input$slider_gamma_bi,   # default: 0.1
-			gamma2   = input$slider_gamma_bi,
-			sigma1   = input$slider_sigma_bi,   # default: 0.1
-			sigma2   = input$slider_sigma_bi,
-			epsilon1 = input$slider_epsilon_bi, # default: 0.1
-			epsilon2 = input$slider_epsilon_bi,
-			delta1   = input$slider_delta_bi,   # default: 0.1
-			delta2   = input$slider_delta_bi
-		)
+  # TS Replication ####
+  output$replication_time_series <- renderPlot({
+    model <- function(t, state, parms) {
+      with(as.list(c(state,parms)), {
+        dR1 <- kappa * RNA1 * replicase * (1 - RNA1) - gamma * RNA1
+        dp1 <-  alpha * RNA1 * (1 - replicase) - sigma * replicase
+        return(list(c(dR1, dp1)))
+      })
+    }
 
-		s <- c(
-			RNA1 = input$slider_r1_bi,
-			RNA2 = input$slider_r2_bi,
-			replicase = 0,
-			coat = 0,
-			virion1 = 0,
-			virion2 = 0
-		)
+    p <- c(kappa = input$slider_kappa,
+           alpha = input$slider_alpha,
+           gamma = input$slider_gamma,
+           sigma = input$slider_sigma)
 
-		cube(x = 1, y = 2, z = 3, state = s)
-	})
 
-	# PP Bipartite ####
-	output$bipartite_phase_port <- renderPlot({
-		model <- function(t, state, parms) {
-			with(as.list(c(state,parms)), {
-				dR1 <- kappa1 * RNA1 * replicase * (1 - RNA1- RNA2) - gamma1 * RNA1 - epsilon1 * RNA1 * coat
-				dR2 <- kappa2 * RNA2 * replicase * (1 - RNA1- RNA2) - gamma2 * RNA2 - epsilon2 * RNA2 * coat
-				dp1 <-  alpha * RNA1 * (1 - replicase - coat) - sigma1 * replicase
-				ds1 <-  beta * RNA2 * (1 - replicase - coat) - sigma2 * coat
-				dv1 <- epsilon1 * RNA1 * coat - delta1 * virion1
-				dv2 <- epsilon2 * RNA2 * coat - delta2 * virion2
-				return(list(c(dR1, dR2, dp1, ds1, dv1, dv2)))
-			})
-		}
+    s <- c(RNA1 = input$slider_r1,
+           replicase = 0)
 
-		p <- c(
-			kappa1   = input$slider_kappa_bi,   # default: 1
-			kappa2   = input$slider_kappa_bi,
-			alpha    = input$slider_alpha_bi,   # default: 1
-			beta     = input$slider_alpha_bi,
-			gamma1   = input$slider_gamma_bi,   # default: 0.1
-			gamma2   = input$slider_gamma_bi,
-			sigma1   = input$slider_sigma_bi,   # default: 0.1
-			sigma2   = input$slider_sigma_bi,
-			epsilon1 = input$slider_epsilon_bi, # default: 0.1
-			epsilon2 = input$slider_epsilon_bi,
-			delta1   = input$slider_delta_bi,   # default: 0.1
-			delta2   = input$slider_delta_bi
-		)
+    run(tstep = 0.5, state = s, odes = model, parms = p)
+  })
 
-		s <- c(
-			RNA1 = input$slider_r1_bi,
-			RNA2 = input$slider_r2_bi,
-			replicase = 0,
-			coat = 0,
-			virion1 = 0,
-			virion2 = 0
-		)
+  # PP Replication ####
+  output$replication_phase_port <- renderPlot({
+    model <- function(t, state, parms) {
+      with(as.list(c(state,parms)), {
+        dR1 <- kappa * RNA1 * replicase * (1 - RNA1) - gamma * RNA1
+        dp1 <-  alpha * RNA1 * (1 - replicase) - sigma * replicase
+        return(list(c(dR1, dp1)))
+      })
+    }
 
-		plane(x = "RNA1", y = "RNA2", tstep = 0.5, state = s, odes = model, parms = p, portrait = T)
-	})
+    p <- c(kappa = input$slider_kappa,
+           alpha = input$slider_alpha,
+           gamma = input$slider_gamma,
+           sigma = input$slider_sigma)
 
-	# Debugger
-	output$debugger <- renderText({
-		paste(input$plots_checkbox)
-	})
+
+    s <- c(RNA1 = input$slider_r1,
+           replicase = 0)
+
+    plane(x = "RNA1", y = "replicase", tstep = 0.5, state = s, odes = model, parms = p, portrait = T)
+  })
+
+  # TS Bipartite ####
+  output$bipartite_time_series <- renderPlot({
+    model <- function(t, state, parms) {
+      with(as.list(c(state,parms)), {
+        dR1 <- kappa1 * RNA1 * replicase * (1 - RNA1- RNA2) - gamma1 * RNA1 - epsilon1 * RNA1 * coat
+        dR2 <- kappa2 * RNA2 * replicase * (1 - RNA1- RNA2) - gamma2 * RNA2 - epsilon2 * RNA2 * coat
+        dp1 <-  alpha * RNA1 * (1 - replicase - coat) - sigma1 * replicase
+        ds1 <-  beta * RNA2 * (1 - replicase - coat) - sigma2 * coat
+        dv1 <- epsilon1 * RNA1 * coat - delta1 * virion1
+        dv2 <- epsilon2 * RNA2 * coat - delta2 * virion2
+        return(list(c(dR1, dR2, dp1, ds1, dv1, dv2)))
+      })
+    }
+
+    p <- c(
+      kappa1   = input$slider_kappa_bi,   # default: 1
+      kappa2   = input$slider_kappa_bi,
+      alpha    = input$slider_alpha_bi,   # default: 1
+      beta     = input$slider_alpha_bi,
+      gamma1   = input$slider_gamma_bi,   # default: 0.1
+      gamma2   = input$slider_gamma_bi,
+      sigma1   = input$slider_sigma_bi,   # default: 0.1
+      sigma2   = input$slider_sigma_bi,
+      epsilon1 = input$slider_epsilon_bi, # default: 0.1
+      epsilon2 = input$slider_epsilon_bi,
+      delta1   = input$slider_delta_bi,   # default: 0.1
+      delta2   = input$slider_delta_bi
+    )
+
+    s <- c(
+      RNA1 = input$slider_r1_bi,
+      RNA2 = input$slider_r2_bi,
+      replicase = 0,
+      coat = 0,
+      virion1 = 0,
+      virion2 = 0
+    )
+
+    run(tstep = 0.5, state = s, odes = model, parms = p)
+  })
+
+  # Traj Bipartite ####
+  output$bipartite_3d_traj <- renderPlot({
+    model <- function(t, state, parms) {
+      with(as.list(c(state,parms)), {
+        dR1 <- kappa1 * RNA1 * replicase * (1 - RNA1- RNA2) - gamma1 * RNA1 - epsilon1 * RNA1 * coat
+        dR2 <- kappa2 * RNA2 * replicase * (1 - RNA1- RNA2) - gamma2 * RNA2 - epsilon2 * RNA2 * coat
+        dp1 <-  alpha * RNA1 * (1 - replicase - coat) - sigma1 * replicase
+        ds1 <-  beta * RNA2 * (1 - replicase - coat) - sigma2 * coat
+        dv1 <- epsilon1 * RNA1 * coat - delta1 * virion1
+        dv2 <- epsilon2 * RNA2 * coat - delta2 * virion2
+        return(list(c(dR1, dR2, dp1, ds1, dv1, dv2)))
+      })
+    }
+
+    p <- c(
+      kappa1   = input$slider_kappa_bi,   # default: 1
+      kappa2   = input$slider_kappa_bi,
+      alpha    = input$slider_alpha_bi,   # default: 1
+      beta     = input$slider_alpha_bi,
+      gamma1   = input$slider_gamma_bi,   # default: 0.1
+      gamma2   = input$slider_gamma_bi,
+      sigma1   = input$slider_sigma_bi,   # default: 0.1
+      sigma2   = input$slider_sigma_bi,
+      epsilon1 = input$slider_epsilon_bi, # default: 0.1
+      epsilon2 = input$slider_epsilon_bi,
+      delta1   = input$slider_delta_bi,   # default: 0.1
+      delta2   = input$slider_delta_bi
+    )
+
+    s <- c(
+      RNA1 = input$slider_r1_bi,
+      RNA2 = input$slider_r2_bi,
+      replicase = 0,
+      coat = 0,
+      virion1 = 0,
+      virion2 = 0
+    )
+
+    cube(x = 1, y = 2, z = 3, state = s)
+  })
+
+  # PP Bipartite ####
+  output$bipartite_phase_port <- renderPlot({
+    model <- function(t, state, parms) {
+      with(as.list(c(state,parms)), {
+        dR1 <- kappa1 * RNA1 * replicase * (1 - RNA1- RNA2) - gamma1 * RNA1 - epsilon1 * RNA1 * coat
+        dR2 <- kappa2 * RNA2 * replicase * (1 - RNA1- RNA2) - gamma2 * RNA2 - epsilon2 * RNA2 * coat
+        dp1 <-  alpha * RNA1 * (1 - replicase - coat) - sigma1 * replicase
+        ds1 <-  beta * RNA2 * (1 - replicase - coat) - sigma2 * coat
+        dv1 <- epsilon1 * RNA1 * coat - delta1 * virion1
+        dv2 <- epsilon2 * RNA2 * coat - delta2 * virion2
+        return(list(c(dR1, dR2, dp1, ds1, dv1, dv2)))
+      })
+    }
+
+    p <- c(
+      kappa1   = input$slider_kappa_bi,   # default: 1
+      kappa2   = input$slider_kappa_bi,
+      alpha    = input$slider_alpha_bi,   # default: 1
+      beta     = input$slider_alpha_bi,
+      gamma1   = input$slider_gamma_bi,   # default: 0.1
+      gamma2   = input$slider_gamma_bi,
+      sigma1   = input$slider_sigma_bi,   # default: 0.1
+      sigma2   = input$slider_sigma_bi,
+      epsilon1 = input$slider_epsilon_bi, # default: 0.1
+      epsilon2 = input$slider_epsilon_bi,
+      delta1   = input$slider_delta_bi,   # default: 0.1
+      delta2   = input$slider_delta_bi
+    )
+
+    s <- c(
+      RNA1 = input$slider_r1_bi,
+      RNA2 = input$slider_r2_bi,
+      replicase = 0,
+      coat = 0,
+      virion1 = 0,
+      virion2 = 0
+    )
+
+    plane(x = "RNA1", y = "RNA2", tstep = 0.5, state = s, odes = model, parms = p, portrait = T)
+  })
+
+  # Debugger
+  output$debugger <- renderText({
+    paste(input$plots_checkbox)
+  })
 
 }
