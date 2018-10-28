ee549156cbf57feceb4a4e76e9ca8ac1a46c1b41
Alfredo Hernández <aldomann.designs@gmail.com>
2018-10-28 13:15:43 +0100

    Major cleaning of the code

--- shiny-app/server.R
+++ shiny-app/server.R
@@ -7,107 +7,12 @@ library(shinyjs)
 
 server <- function(input, output) {
 
-  # Fitting A ####
-  input.vals <- reactiveValues()
-
-  observe({
-    input.vals$dose <- as.numeric(unlist(strsplit(input$dose, ",")))
-    input.vals$aberr <- as.numeric(unlist(strsplit(input$aberr, ",")))
-    input.vals$cells <- as.numeric(unlist(strsplit(input$cells, ",")))
-  })
-
-  output$table <- renderTable({
-    data.frame(
-      Dose = input.vals$dose,
-      Aberrations = input.vals$aberr,
-      Cells = input.vals$cells
-      )
-  })
-
-  # Calculations ####
-  output.vals <- reactiveValues()
-
-  calculations <- observe({
-    dose <- input.vals$dose
-    aberr <- input.vals$aberr
-    cells <- input.vals$cells
-
-    x0 <- cells
-    x1 <- cells * dose
-    x2 <- cells * dose * dose
-    modelldaten <- list(x0, x1, x2, aberr)
-
-    # result <- glm(aberr ~  -1 + x1 + x2,
-    #               family = poisson(link = "identity"),
-    #               data = modelldaten
-    # )
-
-    result <- glm(aberr ~ -1 + x0 + x1 + x2,
-                  family = poisson(link = "identity"),
-                  data = modelldaten
-    )
-
-    smry <- summary(result, correlation = TRUE)
-    smry$coefficients
-    smry$correlation
-
-    corma <- smry$correlation
-    bstat <- smry$coefficients
-    seb <- bstat[, 3]
-    vakoma <- corma * outer(seb, seb)
-    vakoma <- vcov(result)
-
-    output.vals$result <- result
-    output.vals$bstat <- bstat
-    output.vals$vakoma <- vakoma
-    output.vals$corma <- corma
-  })
-
-  # Output prints ####
-  output$result <- renderPrint({
-    # "Result of curve fit 'result'"
-    print(output.vals$result)
-  })
-
-  output$bstat <- renderPrint({
-    # "Coefficients 'bstat'"
-    print(output.vals$bstat)
-  })
-
-  output$vakoma <- renderPrint({
-
-    # "variance-covariance matrix 'vakoma'"
-    print(output.vals$vakoma)
-  })
-
-  output$corma <- renderPrint({
-    # "Correlation matrix 'corma'"
-    print(output.vals$corma)
-  })
-
-
-  # Modular B ####
+  # Fitting ####
   callModule(module = fittingTable, id = "model-b", reactive(input$button_fit))
   callModule(module = fittingResults, id = "model-b", reactive(input$button_fit))
 
-  # fittingServer <- callModule(module = fittingTable, id = "model-b", reactive(input$button_fit))
-  #
-  # output$table_b <- renderTable({
-  #   fittingServer()
-  # })
-  #
-  # fittingServerB <- callModule(fittingResults, "model-b", reactive(input$button_fit))
-  #
-  # output$testtt <- renderPrint({
-  #   fittingServerB()
-  # })
-
-
-
-
-
-  # Test Code ####
-  output$dose_num_value <- renderPrint({ input$dose_num })
+  # Advanced Fitting ####
+  callModule(module = fittingAdvTest, id = "model-c")
 
   # Legacy Code ####
 
--- shiny-app/ui.R
+++ shiny-app/ui.R
@@ -31,7 +31,6 @@ sidebar <- dashboardSidebar(
   sidebarMenu(
     id = "sidebarmenu",
     menuItem("About this App", tabName = "home",  icon = icon("home"), selected = F),
-    # menuItem("Dose-effect Fitting (A)", tabName = "tab-fitting-a",  icon = icon("th-list"), selected = F),
     menuItem("Dose-effect Fitting", tabName = "tab-fitting-b",  icon = icon("th-list"), selected = T),
     menuItem("Advanced Fitting", tabName = "tab-fitting-c",  icon = icon("th-list"), selected = F),
     menuItem("Dose Estimation", tabName = "tab-estimate", icon = icon("calculator")),
@@ -53,7 +52,7 @@ body <- dashboardBody(
     tags$style(HTML("@import url('//fonts.googleapis.com/css?family=Roboto:400,500,700|Roboto+Slab:400,700)"))
   ),
 
-
+  # Home ####
   tabItems(
     tabItem(tabName = "home",
             h2("About this project"),
@@ -72,104 +71,19 @@ body <- dashboardBody(
               )
     ),
 
-    # Fitting A ####
-    tabItem(tabName = "tab-fitting-a",
-            h2("Dose-effect Fitting"),
-            fluidRow(
-              # Sidebar with input and data
-              column(width = 4,
-                     box(width = 12,
-                         title = "Inputs",
-                         status = "primary", solidHeader = F, collapsible = T,
-                         # Inputs
-                         textInput(inputId = "dose",
-                                   label = "Dose",
-                                   value = "0,0.1,0.25,0.5,0.75,1,1.5,2,3,4,5"),
-                         textInput(inputId = "aberr",
-                                   label = "Aberrations",
-                                   value = "8,14,22,55,100,109,100,103,108,103,107"),
-                         textInput(inputId = "cells",
-                                   label = "Cells",
-                                   value = "5000,5002,2008,2002,1832,1168,562,332,193,103,59"),
-                         # Tooltips
-                         bsTooltip("dose", "List of doses",
-                                   "right", options = list(container = "body")),
-                         bsTooltip("aberr", "Aberrations count",
-                                   "right", options = list(container = "body")),
-                         bsTooltip("cells", "Cells count",
-                                   "right", options = list(container = "body"))
-                     ),
-                     box(width = 12,
-                         title = "Data",
-                         status = "primary", solidHeader = F, collapsible = T, collapsed = T,
-                         tableOutput('table')
-                     )
-              ),
-              # Main tabBox
-              column(width = 8,
-                     tabBox(width = 12,
-                            side = "left",
-                            # height = "500px",
-                            # selected = "Tab3",
-                            tabPanel("Result of curve fit", verbatimTextOutput("result")),
-                            tabPanel("Coefficients", verbatimTextOutput("bstat")),
-                            tabPanel("Variance-covariance matrix", verbatimTextOutput("vakoma")),
-                            tabPanel("Correlation matrix", verbatimTextOutput("corma"))
-                     )
-              )
 
-            )
-    ),
-
-
-    # Modular B ####
+    # Fitting  ####
     fittingUI(id = "model-b", label = "tab-fitting-b"),
 
-    # Fitting C ####
-    tabItem(tabName = "tab-fitting-c",
-            h2("Dose-effect Fitting"),
-            fluidRow(
-              # Sidebar with input and data
-              column(width = 12,
-                     box(width = 12,
-                         title = "Inputs",
-                         status = "primary", solidHeader = F, collapsible = T,
-                         numericInput("dose_num", "Dose", value = 1),
-                         fluidRow(column(3, verbatimTextOutput("dose_num_value")))
-                     )
-                     # box(width = 12,
-                     #     title = "Data",
-                     #     status = "primary", solidHeader = F, collapsible = T, collapsed = T,
-                     #     tableOutput('table')
-                     # )
-              )#,
-              # Main tabBox
-              # column(width = 8,
-              #        tabBox(width = 12,
-              #               side = "left",
-              #               # height = "500px",
-              #               # selected = "Tab3",
-              #               tabPanel("Result of curve fit", verbatimTextOutput("result")),
-              #               tabPanel("Coefficients", verbatimTextOutput("bstat")),
-              #               tabPanel("Variance-covariance matrix", verbatimTextOutput("vakoma")),
-              #               tabPanel("Correlation matrix", verbatimTextOutput("corma"))
-              #        )
-              # )
+    # Advanced Fitting ####
+    fittingAdvUI(id = "model-c", label = "tab-fitting-c"),
 
-            )
-    ),
 
-    # Model C ####
+    # Dose Estimation ####
     tabItem(tabName = "tab-estimate",
             h2("Dose Estimation")
     )
 
-  ),
-
-  fluidRow(
-    column(width = 12,
-           uiOutput('ui_plots')
-    )
   )
 )
 
