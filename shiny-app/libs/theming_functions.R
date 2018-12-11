40fe6df6c7f6eec8e2bb97e7e63111edbf01d675
Alfredo Hernández <aldomann.designs@gmail.com>
2018-12-12 00:24:35 +0100

    Added header from original dashboardthemes function

--- shiny-app/libs/theming_functions.R
+++ shiny-app/libs/theming_functions.R
@@ -1,3 +1,34 @@
+### ReadMe ------------------------------------------------------------------
+### Purpose:        This script provides functions required for customising themes
+###                 in shinydashboard applications.
+###
+### Contact:        Nik Lilovski (nik.lilovski@outlook.com)
+###
+### Last updated:   04 March 2018
+###
+#' dashboardthemes: a package for custom themes in shinydashboard applications
+#'
+#' The dashboardthemes package provides two main important features:
+#' \itemize{
+#'   \item Using new pre-defined themes and logos for dashboards.
+#'   \item Creating custom themes and logos for dashboards.
+#' }
+#'
+#' @section Available functions:
+#' The available functions are:
+#' \itemize{
+#'   \item \code{\link{shinyDashboardThemes}}: Calls a custom theme created using shinyDashboardThemeDIY.
+#'   \item \code{\link{shinyDashboardLogo}}: Calls a custom logo created using shinyDashboardLogoDIY.
+#'   \item \code{\link{shinyDashboardThemeDIY}}: Creates a custom theme object.
+#'   \item \code{\link{shinyDashboardLogoDIY}}: Creates a custom logo object. Inspired from \href{http://www.dataseries.org/}{here}.
+#'   \item \code{\link{cssGradientThreeColors}}: Creates a three colour gradient to be used in themes.
+#' }
+#'
+#' @docType package
+#' @name dashboardthemes
+NULL
+
+
 # Function: cssGradientThreeColors ----------------------------------------
 #' Generates CSS code for a gradient with three colors
 #'
