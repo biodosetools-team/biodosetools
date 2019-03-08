5fcbc24c25bb6fbf1eb3822f9f6af76822a386ec
Alfredo Hernández <aldomann.designs@gmail.com>
2019-03-08 01:51:30 +0100

    Clean old functions from server

--- shiny-app-bs4/server.R
+++ shiny-app-bs4/server.R
@@ -10,7 +10,6 @@ library(ggplot2)
 
 server <- function(input, output) {
 
-
   # Fitting ----
   callModule(module = fittingTable, id = "fitting", reactive(input$button_fit))
   callModule(module = fittingResults, id = "fitting", reactive(input$button_fit))
@@ -23,68 +22,5 @@ server <- function(input, output) {
   callModule(module = estimateHotTable, id = "estimate")
   callModule(module = estimateFittingCurve, id = "estimate")
   callModule(module = estimateResults, id = "estimate")
-
-
-
-  output$plot <- renderPlot({
-    hist(rnorm(input$obs))
-  })
-
-  output$distPlot <- renderPlot({
-    dist <- switch(input$dist,
-                   norm = rnorm,
-                   unif = runif,
-                   lnorm = rlnorm,
-                   exp = rexp,
-                   rnorm)
-
-    hist(dist(500))
-  })
-
-  output$plot2 <- renderPlotly({
-    p <- plot_ly(df, x = ~x) %>%
-      add_lines(y = ~y1, name = "A") %>%
-      add_lines(y = ~y2, name = "B", visible = F) %>%
-      layout(
-        xaxis = list(domain = c(0.1, 1)),
-        yaxis = list(title = "y"),
-        updatemenus = list(
-          list(
-            y = 0.8,
-            buttons = list(
-
-              list(method = "restyle",
-                   args = list("line.color", "blue"),
-                   label = "Blue"),
-
-              list(method = "restyle",
-                   args = list("line.color", "red"),
-                   label = "Red"))),
-
-          list(
-            y = 0.7,
-            buttons = list(
-              list(method = "restyle",
-                   args = list("visible", list(TRUE, FALSE)),
-                   label = "Sin"),
-
-              list(method = "restyle",
-                   args = list("visible", list(FALSE, TRUE)),
-                   label = "Cos")))
-        )
-      )
-  })
-
-  output$plot3 <- renderPlotly({
-    s <- subplot(
-      plot_ly(x = x, type = "histogram"),
-      plotly_empty(),
-      plot_ly(x = x, y = y, type = "histogram2dcontour"),
-      plot_ly(y = y, type = "histogram"),
-      nrows = 2, heights = c(0.2, 0.8), widths = c(0.8, 0.2), margin = 0,
-      shareX = TRUE, shareY = TRUE, titleX = FALSE, titleY = FALSE
-    )
-    p <- layout(s, showlegend = FALSE)
-  })
 
 }
