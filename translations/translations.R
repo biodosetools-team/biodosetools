23aabd082468e7e98362f514659c30b6c643110e
Alfredo Hernández <aldomann.designs@gmail.com>
2019-05-21 19:53:25 +0200

    Automatize i18n flags object generation. Display only English language until full shiny.i18n support is added

--- translations/translations.R
+++ translations/translations.R
@@ -6,17 +6,17 @@ library(shiny.i18n)
 # Locales --------------------------------------------------
 
 countries <- c(
-  "en" = "English",
-  "es" = "Spanish",
-  "de" = "German",
-  "fr" = "French"
+  "gb" = "English"#,
+  # "es" = "Spanish",
+  # "de" = "German",
+  # "fr" = "French"
 )
 
-flags <- c(
-  "https://cdn.rawgit.com/lipis/flag-icon-css/master/flags/4x3/gb.svg",
-  "https://cdn.rawgit.com/lipis/flag-icon-css/master/flags/4x3/es.svg",
-  "https://cdn.rawgit.com/lipis/flag-icon-css/master/flags/4x3/de.svg",
-  "https://cdn.rawgit.com/lipis/flag-icon-css/master/flags/4x3/fr.svg"
+flags <- paste(
+  "https://cdn.rawgit.com/lipis/flag-icon-css/master/flags/4x3/",
+  names(countries),
+  ".svg",
+  sep = ""
 )
 
 
@@ -25,4 +25,4 @@ flags <- c(
 # File with translations
 i18n <- Translator$new(translation_csvs_path = "translations")
 # Set language
-i18n$set_translation_language("it")
+i18n$set_translation_language("en")
