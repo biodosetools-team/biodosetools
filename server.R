98b923f24486b4d4adc8ee4cd2b78adc3ab3de44
Alfredo Hernández <aldomann.designs@gmail.com>
2019-06-25 07:51:29 +0200

    New generalEstimateCaseHotTable() module

--- server.R
+++ server.R
@@ -17,7 +17,7 @@ server <- function(input, output) {
   callModule(id = "dicent_fitting", module = generalFittingResults, aberr_module = "dicentrics")
 
   # Dose Estimation
-  callModule(id = "dicent_estimate", module = dicentEstimateCaseHotTable)
+  callModule(id = "dicent_estimate", module = generalEstimateCaseHotTable, aberr_module = "dicentrics")
   callModule(id = "dicent_estimate", module = generalEstimateFittingCurveHotTables)
   callModule(id = "dicent_estimate", module = generalEstimateFittingCurve, aberr_module = "dicentrics")
   callModule(id = "dicent_estimate", module = dicentEstimateResults)
@@ -32,11 +32,10 @@ server <- function(input, output) {
   callModule(id = "trans_fitting", module = generalFittingResults, aberr_module = "translocations", fraction_value = transFittingFraction)
 
   # Dose Estimation
-  callModule(id = "trans_estimate", module = transEstimateCaseHotTable)
-
   callModule(id = "trans_estimate", module = transChromosomeTable)
   transEstimateFraction <-
     callModule(id = "trans_estimate", module = transFractionToFullGenomeCalc)
+  callModule(id = "trans_estimate", module = generalEstimateCaseHotTable, aberr_module = "translocations", fraction_value = transEstimateFraction)
 
   callModule(id = "trans_estimate", module = generalEstimateFittingCurveHotTables)
   callModule(id = "trans_estimate", module = generalEstimateFittingCurve, aberr_module = "translocations")
