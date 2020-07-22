059d707f5ec37387423135ff5d53f289645759d3
Alfredo Hernández <aldomann.designs@gmail.com>
2020-07-22 21:44:25 +0100

    Using rlang::.data$ to avoid "no visible binding for global variable" warnings

--- R/translocations.R
+++ R/translocations.R
@@ -7,6 +7,7 @@
 #'
 #' @return Fraction of genome hybridized.
 #' @export
+#' @importFrom rlang .data
 get_genome_fraction <- function(dna_table, chromosome, color, sex) {
   # Construct color/chromosome table
   color_table <- cbind(
@@ -20,16 +21,18 @@ get_genome_fraction <- function(dna_table, chromosome, color, sex) {
 
   # Full table
   full_table <- dplyr::inner_join(color_table, dna_table, by = "chromosome") %>%
-    dplyr::group_by(color) %>%
-    dplyr::summarise(genome_fraction = sum(base::get(paste0("fraction_", sex))))
+    dplyr::group_by(.data$color) %>%
+    dplyr::summarise(
+      genome_fraction = sum(base::get(paste0("fraction_", sex)))
+    )
 
   # Calculate first sum
   single_sum <- full_table %>%
-    dplyr::select(genome_fraction) %>%
+    dplyr::select(.data$genome_fraction) %>%
     dplyr::summarise(
-      single_sum = sum(genome_fraction * (1 - genome_fraction))
+      single_sum = sum(.data$genome_fraction * (1 - .data$genome_fraction))
     ) %>%
-    dplyr::pull(single_sum)
+    dplyr::pull(.data$single_sum)
 
   # Calculate second sum
   if (nrow(full_table) >= 2) {
@@ -37,9 +40,10 @@ get_genome_fraction <- function(dna_table, chromosome, color, sex) {
       utils::combn(2) %>%
       t() %>%
       as.data.frame() %>%
-      dplyr::summarise(sum(V1 * V2)) %>%
-      unname() %>%
-      unlist()
+      dplyr::summarise(
+        cross_sum = sum(.data$V1 * .data$V2)
+      ) %>%
+      dplyr::pull(.data$cross_sum)
   } else {
     cross_sum <- 0
   }
