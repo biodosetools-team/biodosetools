4359fc56356aa449a91a2e4a32bbaa4e5f2636b2
Alfredo Hernández <aldomann.designs@gmail.com>
2020-10-09 16:47:41 +0100

    Initial implementation of PDF reports

--- R/mod_dicent_fitting_ui.R
+++ R/mod_dicent_fitting_ui.R
@@ -350,8 +350,8 @@ mod_dicent_fitting_ui <- function(id, label) {
               ns("save_report_format"),
               label = NULL,
               width = "85px",
-              choices = list(".html", ".docx"),
-              selected = ".html"
+              choices = list(".pdf", ".html", ".docx"),
+              selected = ".pdf"
             )
           )
         )
