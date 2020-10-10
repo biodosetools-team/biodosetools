41413fd6394c004316c036260ae736a7c1fec571
Alfredo Hernández <aldomann.designs@gmail.com>
2020-10-10 23:55:52 +0100

    New fix_count_data_names() function to improve column names in PDF reports

--- R/utils.R
+++ R/utils.R
@@ -5,7 +5,7 @@
 
 #' Fix coefficient matrix names
 #'
-#' Display coefficient names (`coeff_C`, `coeff_alpha`, `coeff_beta`) to display properly on reports.
+#' Fix coefficient names (`coeff_C`, `coeff_alpha`, `coeff_beta`) to display properly on reports.
 #'
 #' @param data Data frame or matrix.
 #' @param type Type of name replacement. Either "rows" or "cols".
@@ -43,6 +43,56 @@ fix_coeff_names <- function(data, type = c("rows", "cols"), output = c("kable", 
         gsub(coeffs_old[[2]], coeffs_new[[2]], .) %>%
         gsub(coeffs_old[[3]], coeffs_new[[3]], .)
     )
+  return(data)
+}
+
+#' Fix data frame variable names
+#'
+#' Fix data colnames to display properly on reports.
+#'
+#' @param data Data frame or matrix.
+#' @param type Type of input data. Either "count" and "case".
+#' @param output Type of output in which the data is rendered. Only "kable" supported at the moment.
+#'
+#' @return Data with fixed colnames.
+#' @noRd
+fix_count_data_names <- function(data, type = c("count", "case"), output = "kable") {
+  type <- match.arg(type)
+  output <- match.arg(output)
+
+  col_names <- colnames(data)
+
+  counts_headers <- grep("C[0-9]", x = col_names, value = TRUE)
+  counts_headers <- paste0("$C_{", regmatches(counts_headers, regexpr("[0-9]", counts_headers)), "}$")
+
+  if (type == "count") {
+    other_headers <- grep("C[0-9]", x = col_names, value = TRUE, invert = TRUE) %>%
+      gsub("^D$", "$D$ (Gy)", .) %>%
+      gsub("^N$", "$N$", .) %>%
+      gsub("^X$", "$X$", .) %>%
+      gsub("^mean$", "$\\\\bar{y}$", .) %>%
+      gsub("^var$", "$\\\\sigma$", .) %>%
+      gsub("^DI$", "$\\\\sigma^{2} / \\\\bar{y}$", .) %>%
+      gsub("^u$", "$u$", .)
+
+    colnames(data) <- c(other_headers[1:3], counts_headers, other_headers[4:length(other_headers)])
+  } else if (type == "case") {
+    other_headers <- grep("C[0-9]", x = col_names, value = TRUE, invert = TRUE) %>%
+      gsub("^N$", "$N$", .) %>%
+      gsub("^X$", "$X$", .) %>%
+      gsub("^y$", "$y$", .) %>%
+      gsub("^y_err$", "$y_{err}$", .) %>%
+      gsub("^Fp$", "$F_{P}$", .) %>%
+      gsub("^Fp_err$", "$F_{P,err}$", .) %>%
+      gsub("^Fg$", "$F_{G}$", .) %>%
+      gsub("^Xc$", "$X_{C}$", .) %>%
+      gsub("^Fg_err$", "$F_{G,err}$", .) %>%
+      gsub("^DI$", "$\\\\sigma^{2} / \\\\bar{y}$", .) %>%
+      gsub("^u$", "$u$", .)
+
+    colnames(data) <- c(other_headers[1:2], counts_headers, other_headers[3:length(other_headers)])
+  }
+
   return(data)
 }
 
