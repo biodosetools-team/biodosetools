097876346cad983f3826cb737fce944d7f5d783d
Alfredo Hernández <aldomann.designs@gmail.com>
2021-03-21 16:51:13 +0000

    Added unit test for translocations fitting

--- tests/testthat/test-translocations.R
+++ tests/testthat/test-translocations.R
@@ -1,3 +1,5 @@
+# Translocation utils ----
+
 test_that("get_translocation_rate works", {
   # Default parameters for confounders
   default_rate <- get_translocation_rate(
@@ -40,3 +42,116 @@ test_that("get_genome_fraction works", {
   # Expected output
   expect_equal(round(genome_fraction, 3), 0.585)
 })
+
+
+# Translocations fitting ----
+
+test_that("get_fit_results with full count data works", {
+  # Example from IAEA (2011)
+  trans_count_data <- app_sys("extdata", "count-data-IAEA.csv") %>%
+    read.csv() %>%
+    calculate_aberr_table(type = "count")
+
+  # Expected outputs
+  expect_equal(trans_count_data$N, c(5000L, 5002L, 2008L, 2002L, 1832L, 1168L, 562L, 333L, 193L, 103L, 59L))
+  expect_equal(trans_count_data$X, c(8L, 14L, 22L, 55L, 100L, 109L, 100L, 103L, 108L, 103L, 107L))
+  expect_equal(which(unname(trans_count_data$u) > 1.96), c(3))
+
+  # Fitting (glm)
+  model_formula <- list_fitting_formulas()[[1]]
+  model_family <- "automatic"
+  aberr_module <- "translocations"
+
+  fit_results_list <- get_fit_results(
+    count_data = trans_count_data,
+    model_formula,
+    model_family,
+    fit_link = "identity",
+    aberr_module
+  )
+
+  gg_curve <- get_fit_dose_curve(
+    fit_results_list,
+    aberr_name = to_title(aberr_module)
+  )
+
+  # Expected outputs
+  expect_gt(ncol(trans_count_data), 3)
+  expect_equal(fit_results_list$fit_raw_data, as.matrix(trans_count_data))
+  expect_equal(fit_results_list$fit_algorithm, "glm")
+  expect_equal(round(unname(fit_results_list$fit_model_statistics[, "logLik"]), 2), -4.66)
+  expect_equal(gg_curve$data$dose, trans_count_data$D)
+
+  # Fitting (maxlik)
+  model_formula <- list_fitting_formulas()[[2]]
+
+  # Expected glm warning in tryCatch()
+  expect_warning(
+    get_fit_results(
+      count_data = trans_count_data,
+      model_formula,
+      model_family,
+      fit_link = "identity",
+      aberr_module
+    )
+  )
+})
+
+test_that("get_fit_results with aggregated count data works", {
+  # Example from IAEA (2011)
+  trans_count_data <- app_sys("extdata", "count-data-aggr-IAEA.csv") %>%
+    read.csv() %>%
+    dplyr::mutate(
+      D = as.numeric(.data$D)
+    )
+
+  # Expected outputs
+  expect_equal(ncol(trans_count_data), 3)
+  expect_equal(trans_count_data$N, c(5000L, 5002L, 2008L, 2002L, 1832L, 1168L, 562L, 333L, 193L, 103L, 59L))
+  expect_equal(trans_count_data$X, c(8L, 14L, 22L, 55L, 100L, 109L, 100L, 103L, 108L, 103L, 107L))
+
+  # Fitting (glm)
+  model_formula <- list_fitting_formulas()[[1]]
+  model_family <- "automatic"
+  aberr_module <- "translocations"
+
+  fit_results_list <- get_fit_results(
+    count_data = trans_count_data,
+    model_formula,
+    model_family,
+    fit_link = "identity",
+    aberr_module
+  )
+
+  gg_curve <- get_fit_dose_curve(
+    fit_results_list,
+    aberr_name = to_title(aberr_module)
+  )
+
+  # Expected outputs
+  expect_equal(fit_results_list$fit_raw_data, as.matrix(trans_count_data))
+  expect_equal(fit_results_list$fit_algorithm, "glm")
+  expect_equal(round(unname(fit_results_list$fit_model_statistics[, "logLik"]), 2), -4.66)
+  expect_equal(gg_curve$data$dose, trans_count_data$D)
+
+  # Fitting (maxlik)
+  model_formula <- list_fitting_formulas()[[2]]
+
+  # Expected glm warning in tryCatch()
+  expect_warning(
+    get_fit_results(
+      count_data = trans_count_data,
+      model_formula,
+      model_family,
+      fit_link = "identity",
+      aberr_module
+    )
+  )
+})
+
+
+# Translocations dose estimation ----
+
+# test_that("processing aggregated count data works", {
+#
+# })
