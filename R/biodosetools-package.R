13ac38d938944c31ceb2687030778c28fc78d484
Alfredo Hernández <aldomann.designs@gmail.com>
2021-03-21 21:07:42 +0000

    Added "where" to global variables (see https://github.com/r-lib/tidyselect/issues/201)

--- R/biodosetools-package.R
+++ R/biodosetools-package.R
@@ -10,4 +10,10 @@
 NULL
 
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
-if (getRversion() >= "2.15.1") utils::globalVariables(c("."))
+if (getRversion() >= "2.15.1") {
+  utils::globalVariables(".")
+
+  # Fix for tidyselect::where()
+  # See https://github.com/r-lib/tidyselect/issues/201
+  utils::globalVariables("where")
+}
