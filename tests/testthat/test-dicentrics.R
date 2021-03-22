e7c2d283940499e862ed6aec00e79727500da35d
Alfredo Hernández <aldomann.designs@gmail.com>
2021-03-22 23:16:44 +0000

    Added initial test for whole-body dose estimation for dicentrics

--- tests/testthat/test-dicentrics.R
+++ tests/testthat/test-dicentrics.R
@@ -145,4 +145,87 @@ test_that("processing case data works", {
   expect_equal(case_data_cols[1:2], c("N", "X"))
   expect_true(all(grepl("C", case_data_cols[seq(3, case_data_cols_len - 4, 1)])))
   expect_equal(case_data_cols[seq(case_data_cols_len - 3, case_data_cols_len, 1)], c("DI", "u", "y", "y_err"))
+
+  # Dose estimation
+  exposure <- "acute"
+  assessment <- "whole-body"
+  error_method <- "merkle-83"
+
+  fit_results_list <- app_sys("extdata", "dicentrics-fitting-data-2020-10-10.rds") %>%
+    readRDS()
+
+  # Parse fitting data
+  fit_coeffs <- fit_results_list[["fit_coeffs"]]
+  fit_var_cov_mat <- fit_results_list[["fit_var_cov_mat"]]
+  fit_formula_tex <- fit_results_list[["fit_formula_tex"]]
+
+  # Generalised fit coefficients
+  general_fit_coeffs <- generalise_fit_coeffs(fit_coeffs[, "estimate"])
+
+  # Generalised variance-covariance matrix
+  general_var_cov_mat <- generalise_fit_var_cov_mat(fit_var_cov_mat)
+
+  # Protracted variables
+  if (exposure == "protracted") {
+    # protracted_time <-
+    # protracted_life_time <-
+    protracted_g_value <- protracted_g_function(protracted_time, protracted_life_time)
+  } else if (exposure == "protracted_high") {
+    protracted_g_value <- 0
+    # Used in report (dummy values)
+    protracted_time <- NA
+    protracted_life_time <- NA
+  } else {
+    protracted_g_value <- 1
+    # Used in report (dummy values)
+    protracted_time <- NA
+    protracted_life_time <- NA
+  }
+
+  # Confidence intervals
+
+  # Select whole-body CI depending on selected method
+  if (grepl("merkle", error_method, fixed = TRUE)) {
+    conf_int_curve <- paste0("0.", gsub("\\D", "", error_method)) %>% as.numeric()
+    conf_int_yield <- conf_int_curve
+
+    # Parse CI text for plot legend
+    conf_int_text_whole <- paste0(
+      "(", round(100 * conf_int_curve, 0), "%",
+      "-", round(100 * conf_int_yield, 0), "%", ")"
+    )
+  } else if (error_method == "delta") {
+    conf_int_curve <- 0.83
+    conf_int_yield <- conf_int_curve
+    conf_int_delta <- 0.95
+
+    # Parse CI text for plot legend
+    conf_int_text_whole <- paste0("(", round(100 * conf_int_delta, 0), "%", ")")
+  }
+
+  conf_int_curve <- conf_int_curve %>%
+    correct_conf_int(general_var_cov_mat, protracted_g_value, type = "curve")
+  conf_int_yield <- conf_int_yield %>%
+    correct_conf_int(general_var_cov_mat, protracted_g_value, type = "yield")
+
+  # Parse genome fraction
+  parsed_genome_fraction <- 1
+
+  # Calculations
+  results_whole <- estimate_whole_body(
+    case_data,
+    general_fit_coeffs,
+    general_var_cov_mat,
+    conf_int_yield,
+    conf_int_curve,
+    protracted_g_value,
+    parsed_genome_fraction,
+    aberr_module = "dicentrics"
+  )
+
+  # Expected outputs
+  expect_equal(colnames(results_whole$est_doses), c("yield", "dose"))
+  expect_equal(rownames(results_whole$est_doses), c("lower", "estimate", "upper"))
+  expect_equal(round(results_whole$AIC, 3), 7.771)
+
 })
