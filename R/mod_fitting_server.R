622c6fc55aff9d65eed173fc975a0c9e7634cfe3
Alfredo Hernández <aldomann.designs@gmail.com>
2021-05-17 08:04:46 +0100

    Added irr_conds to RDS results_list in mod_fitting_results_server() module

--- R/mod_fitting_server.R
+++ R/mod_fitting_server.R
@@ -431,9 +431,48 @@ mod_fitting_results_server <- function(input, output, session, stringsAsFactors,
     },
     content = function(file) {
       if (input$save_fit_data_format == ".rds") {
+        # Read results_list
         results_list <- data()
+
+        # Add additional values to list
         results_list[["gg_curve"]] <- NULL
+        results_list[["irr_conds"]] <- list(
+          irradiator_name = c(
+            label = "Name of the irradiator used",
+            text = input$irr_cond_irradiator_name
+          ),
+          radiation_quality = c(
+            label = "Radiation quality",
+            text = input$irr_cond_radiation_quality
+          ),
+          dose_rate = c(
+            label = "Dose rate (Gy/min)",
+            text = input$irr_cond_dose_rate
+          ),
+          dose_quantity = c(
+            label = "Dose quantity",
+            text = input$irr_cond_dose_quantity
+          ),
+          whole_blood = c(
+            label = "Whole blood or isolated lymphocytes",
+            text = input$irr_cond_whole_blood
+          ),
+          temperature = c(
+            label = "Temperature",
+            text = input$irr_cond_temperature
+          ),
+          time = c(
+            label = "Time incubations",
+            text = input$irr_cond_time
+          ),
+          beam_characteristics = c(
+            label = "Beam characteristics",
+            text = input$irr_cond_beam_characteristics
+          )
+        )
         results_list[["biodosetools_version"]] <- utils::packageVersion(pkg = 'biodosetools')
+
+        # Export RDS file
         saveRDS(results_list, file = file)
       }
     }
