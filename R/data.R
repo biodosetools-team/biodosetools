ca33ee9439c6ae9f0bf49dc6e33ec068a97bc3f5
Alfredo Hernández <aldomann.designs@gmail.com>
2022-04-06 10:54:01 +0100

    Removed legacy gardner_confidence_table data (deprecated in 585e7b4b6f42e66965e3b0f6a271dbaea7b7cf28)

--- R/data.R
+++ R/data.R
@@ -1,16 +1,3 @@
-#' Gardner's Confidence Intervals Table
-#'
-#' Confidence intervals for the expectation of a Poisson variable.
-#'
-#' @format A data frame with 1201 rows and 3 variables:
-#' \describe{
-#'   \item{s_est}{Nominal value for the Poisson variable.}
-#'   \item{s_low}{Lower confidence interval for the Poisson variable.}
-#'   \item{s_upp}{Upper confidence interval for the Poisson variable.}
-#' }
-#' @source \doi{10.1093/biomet/46.3-4.441}
-"gardner_confidence_table"
-
 #' DNA Content Fractions of Human Chromosomes (Morton 1991)
 #'
 #' Normalised DNA Content of Human Chromosomes from
