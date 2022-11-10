e75acbbfcd27069e80bc7f671956b133515451ac
Alfredo Hernández <aldomann.designs@gmail.com>
2022-11-10 18:09:03 +0000

    Updated app_config.R and test-golem-recommended.R unit tests (changed in https://github.com/ThinkR-open/golem/commit/61b9063f65006bc9b15dd2f1e535466c45d25424)

--- R/app_config.R
+++ R/app_config.R
@@ -11,20 +11,26 @@ app_sys <- function(...) {
 #' Read App Config
 #'
 #' @param value Value to retrieve from the config file.
-#' @param config R_CONFIG_ACTIVE value.
+#' @param config GOLEM_CONFIG_ACTIVE value. If unset, R_CONFIG_ACTIVE.
+#' If unset, "default".
 #' @param use_parent Logical, scan the parent directory for config file.
-#'
-#' @importFrom config get
+#' @param file Location of the config file.
 #'
 #' @noRd
 get_golem_config <- function(value,
-                             config = Sys.getenv("R_CONFIG_ACTIVE", "default"),
-                             use_parent = TRUE) {
+                             config = Sys.getenv(
+                               "GOLEM_CONFIG_ACTIVE",
+                               Sys.getenv(
+                                 "R_CONFIG_ACTIVE",
+                                 "default"
+                               )
+                             ),
+                             use_parent = TRUE,
+                             file = app_sys("golem-config.yml")) {
   config::get(
     value = value,
     config = config,
-    # Modify this if your config file is somewhere else:
-    file = app_sys("golem-config.yml"),
+    file = file,
     use_parent = use_parent
   )
 }
