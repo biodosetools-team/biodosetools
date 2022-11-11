80cbd5efb87560dfe285cbb46f35927cc119544b
Alfredo Hernández <aldomann.designs@gmail.com>
2022-11-11 07:47:18 +0000

    Updated all mod_*_server() modules to use moduleServer(), as recommended in {shiny} >= 1.5.0 (see https://shiny.rstudio.com/articles/modules.html)

--- R/app_server.R
+++ R/app_server.R
@@ -5,141 +5,49 @@
 #' @import shiny
 #' @noRd
 app_server <- function(input, output, session) {
-
   # Dicentrics Modules ----
 
   # Fitting
-  callModule(
-    id = "fitting_dicent_ui",
-    module = mod_fitting_counts_hot_server,
-    aberr_module = "dicentrics"
-  )
-  callModule(
-    id = "fitting_dicent_ui",
-    module = mod_fitting_results_server,
-    aberr_module = "dicentrics"
-  )
+  mod_fitting_counts_hot_server("fitting_dicent_ui", "dicentrics")
+  mod_fitting_results_server("fitting_dicent_ui", "dicentrics")
 
   # Dose Estimation
-  callModule(
-    id = "estimation_dicent_ui",
-    module = mod_estimation_case_hot_server,
-    aberr_module = "dicentrics"
-  )
-  callModule(
-    id = "estimation_dicent_ui",
-    module = mod_estimation_fit_curve_hot_server
-  )
-  callModule(
-    id = "estimation_dicent_ui",
-    module = mod_estimation_fit_curve_server,
-    aberr_module = "dicentrics"
-  )
-  callModule(
-    id = "estimation_dicent_ui",
-    module = mod_estimation_results_server,
-    aberr_module = "dicentrics"
-  )
+  mod_estimation_case_hot_server("estimation_dicent_ui", "dicentrics")
+  mod_estimation_fit_curve_hot_server("estimation_dicent_ui")
+  mod_estimation_fit_curve_server("estimation_dicent_ui", "dicentrics")
+  mod_estimation_results_server("estimation_dicent_ui", "dicentrics")
 
   # Translocations Modules ----
 
   # Help
-  callModule(
-    id = "fitting_trans_ui",
-    module = mod_help_chromosome_hot_server
-  )
-  callModule(
-    id = "estimation_trans_ui",
-    module = mod_help_chromosome_hot_server
-  )
+  mod_help_chromosome_hot_server("fitting_trans_ui")
+  mod_help_chromosome_hot_server("estimation_trans_ui")
 
   # Fitting
-  callModule(
-    id = "fitting_trans_ui",
-    module = mod_trans_chromosome_hot_server
-  )
-  callModule(
-    id = "fitting_trans_ui",
-    module = mod_trans_fraction_to_full_genome_server
-  ) -> transFittingFraction
-  callModule(
-    id = "fitting_trans_ui",
-    module = mod_fitting_counts_hot_server,
-    aberr_module = "translocations"
-  )
-  callModule(
-    id = "fitting_trans_ui",
-    module = mod_fitting_results_server,
-    aberr_module = "translocations",
-    genome_factor = transFittingFraction
-  )
+  mod_trans_chromosome_hot_server("fitting_trans_ui")
+  genome_factor_fit <- mod_trans_fraction_to_full_genome_server("fitting_trans_ui")
+  mod_fitting_counts_hot_server("fitting_trans_ui", "translocations")
+  mod_fitting_results_server("fitting_trans_ui", "translocations", genome_factor_fit)
 
   # Dose Estimation
-  callModule(
-    id = "estimation_trans_ui",
-    module = mod_trans_chromosome_hot_server
-  )
-  callModule(
-    id = "estimation_trans_ui",
-    module = mod_trans_fraction_to_full_genome_server
-  ) -> transEstimateFraction
-  callModule(
-    id = "estimation_trans_ui",
-    module = mod_estimation_case_hot_server,
-    aberr_module = "translocations",
-    genome_factor = transEstimateFraction
-  )
-
-  callModule(
-    id = "estimation_trans_ui",
-    module = mod_estimation_fit_curve_hot_server
-  )
-  callModule(
-    id = "estimation_trans_ui",
-    module = mod_estimation_fit_curve_server,
-    aberr_module = "translocations"
-  )
-  callModule(
-    id = "estimation_trans_ui",
-    module = mod_estimation_results_server,
-    aberr_module = "translocations",
-    genome_factor = transEstimateFraction
-  )
+  mod_trans_chromosome_hot_server("estimation_trans_ui")
+  genome_factor_estimate <- mod_trans_fraction_to_full_genome_server("estimation_trans_ui")
+  mod_estimation_case_hot_server("estimation_trans_ui", "translocations", genome_factor_estimate)
+  mod_estimation_fit_curve_hot_server("estimation_trans_ui")
+  mod_estimation_fit_curve_server("estimation_trans_ui", "translocations")
+  mod_estimation_results_server("estimation_trans_ui", "translocations", genome_factor_estimate)
 
   # Micronuclei Modules ----
 
   if (golem::app_dev()) {
     # Fitting
-    callModule(
-      id = "fitting_micro_ui",
-      module = mod_fitting_counts_hot_server,
-      aberr_module = "micronuclei"
-    )
-    callModule(
-      id = "fitting_micro_ui",
-      module = mod_fitting_results_server,
-      aberr_module = "micronuclei"
-    )
+    mod_fitting_counts_hot_server("fitting_micro_ui", "micronuclei")
+    mod_fitting_results_server("fitting_micro_ui", "micronuclei")
 
     # Dose Estimation
-    callModule(
-      id = "estimation_micro_ui",
-      module = mod_estimation_case_hot_server,
-      aberr_module = "micronuclei"
-    )
-    callModule(
-      id = "estimation_micro_ui",
-      module = mod_estimation_fit_curve_hot_server
-    )
-    callModule(
-      id = "estimation_micro_ui",
-      module = mod_estimation_fit_curve_server,
-      aberr_module = "micronuclei"
-    )
-    callModule(
-      id = "estimation_micro_ui",
-      module = mod_estimation_results_server,
-      aberr_module = "micronuclei"
-    )
+    mod_estimation_case_hot_server("estimation_micro_ui", "micronuclei")
+    mod_estimation_fit_curve_hot_server("estimation_micro_ui")
+    mod_estimation_fit_curve_server("estimation_micro_ui", "micronuclei")
+    mod_estimation_results_server("estimation_micro_ui", "micronuclei")
   }
 }
