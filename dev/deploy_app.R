7b413befc9df09a960fe2b2e72e244cd2d23e7a7
Alfredo Hernández <aldomann.designs@gmail.com>
2023-05-22 21:48:09 +0100

    Removed old biodosetools-v3 deploy_app.R script

--- dev/deploy_app.R
+++
@@ -1,7 +0,0 @@
-# Deploy app.R to ShinyApps
-
-rsconnect::deployApp(
-  appName = "biodosetools-v3",
-  appDir = here::here(),
-  appFileManifest = here::here("dev", "app_file_manifest.txt")
-)
--- dev/deploy_app_redirect.R
+++ dev/deploy_app_redirect.R
@@ -1,7 +1,7 @@
 # Deploy app.R to ShinyApps
 
 rsconnect::deployApp(
-  appName = "biodosetools-redirect",
+  appName = "biodosetools-v3",
   appDir = here::here(),
   appPrimaryDoc = "dev/shiny-redirect/app.R",
   appFileManifest = "dev/shiny-redirect/app_file_manifest.txt"
